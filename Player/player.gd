eab37a5b1cc8a102c0c5ab44cd997b671b46ea01
Christina <47138990+CafCat49@users.noreply.github.com>
2025-10-15 16:00:15 +0200

    percentile lerp

--- Player/player.gd
+++ Player/player.gd
@@ -30,6 +30,7 @@ var _attack_direction := Vector3.ZERO
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	health_component.update_max_health(30.0)
+	print(stats.get_base_speed())
 
 func _physics_process(delta: float) -> void:
 	frame_camera_rotation()
--- Resources/CharacterStats.gd
+++ Resources/CharacterStats.gd
@@ -9,14 +9,27 @@ class Ability:
 		set(value):
 			ability_score = clamp(value, 0, 100)
 			
-	func _init(min: float, max: float) -> void:
-		min_modifier = min
-		max_modifier = max
+	func _init(min_value: float, max_value: float) -> void:
+		min_modifier = min_value
+		max_modifier = max_value
+		
+	func percentile_lerp(min_bound: float, max_bound: float) -> float:
+		return lerp(min_bound, max_bound, ability_score / 100.0)
+		
+	func get_modifier() -> float:
+		return percentile_lerp(min_modifier, max_modifier)
 
 var level := 1
 var xp := 0
 
-var strength
-var speed = Ability.new(3.0, 7.0)
-var endurance
-var agility
+# Damage bonus on attack.
+var strength := Ability.new(2.0, 12.0)
+# Movement speed.
+var speed := Ability.new(3.0, 7.0)
+# HP bonus per level.
+var endurance := Ability.new(5.0, 25.0)
+# Crit chance.
+var agility := Ability.new(0.05, 0.25)
+
+func get_base_speed() -> float:
+	return speed.get_modifier()
