548c6dbb31509548855d8c8254f6073f8d612f57
Christina <47138990+CafCat49@users.noreply.github.com>
2025-10-24 15:38:48 +0200

    Character Sheet Updates

--- UserInterface/inventory.gd
+++ UserInterface/inventory.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var agility_value: Label = %AgilityValue
 @onready var speed_value: Label = %SpeedValue
 @onready var endurance_value: Label = %EnduranceValue
+@onready var damage_value: Label = %DamageValue
 
 @onready var player: Player = get_parent().player
 
@@ -12,7 +13,20 @@ func _ready() -> void:
 	update_stats()
 
 func update_stats() -> void:
+	level_label.text = "Level %s" % player.stats.level
 	strength_value.text = str(player.stats.strength.ability_score)
 	agility_value.text = str(player.stats.agility.ability_score)
 	speed_value.text = str(player.stats.speed.ability_score)
 	endurance_value.text = str(player.stats.endurance.ability_score)
+
+func update_gear_stats() -> void:
+	damage_value.text = str(get_weapon_damage())
+
+func get_weapon_damage() -> int:
+	var damage = 10
+	damage += player.stats.get_damage_modifier()
+	return damage
+
+
+func _on_return_button_pressed() -> void:
+	get_parent().close_menu()
